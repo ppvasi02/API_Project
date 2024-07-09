8157c30572a9c1c2d29a3d5a333de6f47863a726
Pavel <pvasilyev@fordham.edu>
2024-07-09 17:41:07 -0400

    connected API to Atlas server

--- main.go
+++ main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"net/http"
 
 	"errors"
 
 	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type URL struct {
@@ -71,6 +76,26 @@ outerLoop:
 }
 
 func main() {
+	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
+	opts := options.Client().ApplyURI("mongodb+srv://pvasilyev:ccCTkS1UnwiAuxr4@apiproject0.uugqh4j.mongodb.net/?retryWrites=true&w=majority&appName=APIProject0").SetServerAPIOptions(serverAPI)
+
+	// Create a new client and connect to the server
+	client, err := mongo.Connect(context.TODO(), opts)
+	if err != nil {
+		panic(err)
+	}
+
+	defer func() {
+		if err = client.Disconnect(context.TODO()); err != nil {
+			panic(err)
+		}
+	}()
+	// Send a ping to confirm a successful connection
+	if err := client.Database("admin").RunCommand(context.TODO(), bson.D{{"ping", 1}}).Err(); err != nil {
+		panic(err)
+	}
+	fmt.Println("Pinged your deployment. You successfully connected to MongoDB!")
+
 	router := gin.Default()
 	router.GET("/links", getLinks)                // curl localhost:8080/links
 	router.GET("/links/:short_code", requestLink) // curl localhost:8080/links/000002
