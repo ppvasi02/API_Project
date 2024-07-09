b3432eb1f95dd05a66f7a9046f9c3dbf89109d4d
Pavel <pvasilyev@fordham.edu>
2024-07-09 15:37:31 -0400

    fixed createLink()

--- main.go
+++ main.go
@@ -14,6 +14,10 @@ type URL struct {
 	ShortCode string `json:"short_code"`
 }
 
+type code struct {
+	LongURL string `json:"long_url"`
+}
+
 var links = []URL{
 	{LongURL: "https://www.google.com/", ShortCode: "000001"},
 	{LongURL: "https://www.youtube.com/", ShortCode: "000002"},
@@ -23,7 +27,7 @@ func getLinks(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, links)
 }
 
-func addLink(c *gin.Context) {
+func importLink(c *gin.Context) {
 	var newLink URL
 
 	if err := c.BindJSON(&newLink); err != nil {
@@ -41,9 +45,9 @@ func addLink(c *gin.Context) {
 	c.IndentedJSON(http.StatusCreated, newLink)
 }
 
-func requestLong(c *gin.Context) {
+func requestLink(c *gin.Context) {
 	code := c.Param("short_code")
-	URL, err := getLong(code)
+	URL, err := getLink(code)
 
 	if err != nil {
 		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Link not found."})
@@ -53,7 +57,7 @@ func requestLong(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, URL)
 }
 
-func getLong(code string) (*URL, error) {
+func getLink(code string) (*URL, error) {
 	for i, l := range links {
 		if l.ShortCode == code {
 			return &links[i], nil
@@ -62,21 +66,21 @@ func getLong(code string) (*URL, error) {
 	return nil, errors.New("code not found")
 }
 
-func createLink(c *gin.Context) { //HTTP/1.1 404 Not Found
-	var code string
-	if err := c.BindJSON(&code); err != nil {
-		// Handle error (e.g., log the error, return an error response)
-		c.IndentedJSON(http.StatusPreconditionFailed, gin.H{"message": "Could not read input."})
+func createLink(c *gin.Context) {
+	var url code
+
+	if err := c.BindJSON(&url); err != nil {
 		return
 	}
 	for i, l := range links {
-		if l.LongURL == code {
+		if l.LongURL == url.LongURL {
 			c.IndentedJSON(http.StatusFound, links[i])
 			return
 		}
 	}
+
 	var newLink URL
-	newLink.LongURL = code
+	newLink.LongURL = url.LongURL
 	newLink.ShortCode = uuid.New().String()[:6] // Get first 6 characters from UUID
 
 	links = append(links, newLink)
@@ -86,8 +90,8 @@ func createLink(c *gin.Context) { //HTTP/1.1 404 Not Found
 func main() {
 	router := gin.Default()
 	router.GET("/links", getLinks)                // curl localhost:8080/links
-	router.GET("/links/:short_code", requestLong) // curl localhost:8080/links/000002
-	router.POST("/links", addLink)                // curl localhost:8080/links --include --header "Content-Type: application/json" -d @body.json --request "POST"
-	router.POST("/links/", createLink)
+	router.GET("/links/:short_code", requestLink) // curl localhost:8080/links/000002
+	// router.POST("/links", importLink)             // curl localhost:8080/links --include --header "Content-Type: application/json" -d @body.json --request "POST"
+	router.POST("/links", createLink)
 	router.Run("localhost:8080")
 }
